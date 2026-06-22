e62939d9060d47432cc63a4615c6d15d960facf3
jjaemun <jjaemuuu@gmail.com>
2026-06-22 21:58:09 +0200

    lib

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,21 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+#![no_std]
+
+
+use core::convert::Infallible;
+
+
+/// Trait for infallible date validations.
+///
+/// `Source` refines the [`TrySource`] trait for infallible date
+/// validations.
+pub trait Source : TrySource<Error = Infallible> {
+    fn is_valid(&self, date: Self::Date) -> bool;
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub trait TrySource {
+    type Date;
+    type Error: core::error::Error;
+
+    fn try_is_valid(&self, date: Self::Date) -> Result<bool, Self::Error>;
 }
