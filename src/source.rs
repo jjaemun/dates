ce3b996cace1c85055ad64affaec044c5c69ee7c
jjaemun <jjaemuuu@gmail.com>
2026-08-06 15:29:33 +0200

    exper

---
+++ src/source.rs
@@ -0,0 +1,30 @@
+use core::convert::Infallible;
+
+pub enum SourceKind {
+    Weekend,
+    Holiday,
+}
+
+pub trait Source: TrySource<Error = Infallible> {
+    fn contains(&self, date: &Self::Date) -> bool;
+}
+
+impl<S> Source for S
+where 
+    S: TrySource<Error = Infallible>,
+{
+    fn contains(&self, date: &Self::Date) -> bool {
+        match self.try_contains(date) {
+            Ok(contained) => contained,
+        }
+    }
+}  
+
+pub trait TrySource {
+    type Error: core::error::Error;
+    type Date;
+
+    fn kind(&self) -> SourceKind;
+
+    fn try_contains(&self, date: &Self::Date) -> Result<bool, Self::Error>;
+}
